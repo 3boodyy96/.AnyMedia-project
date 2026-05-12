5f406fd886800f94ec32707e05b4045b8a87fcf2
3boodyy96 <3boodyy96@protonmail.com>
2026-05-12 13:57:21 +0300

    :3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import DropDownMenu from '../src/components/dropDownMenu.jsx';
-// import LoginPage from './signin-login_page.jsx';
 export default function App() {
   return (
     <div>
-      
+
     </div>
   )}
