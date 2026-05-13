013f094f6cc0cdce24b9bd153ef825e91e283adf
3boodyy96 <3boodyy96@protonmail.com>
2026-05-13 14:44:35 +0300

    added the basemnts for the home btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import DropDownMenu from "./components/dropDownMenu";
+import HomePageBtn from "./components/HomePageBtn.jsx";
 export default function App() {
-    const handleSelect = (value) => {
-    console.log("Selected:", value);
-    // value will be "edit", "delete", or "share"
-    if (value === "edit")   { /* ... */ }
-    if (value === "delete") { /* ... */ }
-    if (value === "share")  { /* ... */ }
-  };
-  return (
-    <div>
-      
-    <DropDownMenu
-      text="Actions"
-      id="actions-menu"
-      options={["edit", "delete", "share"]}
-      onSelect={handleSelect}
-      ButtonStyle={'w-60!'}
-/>
+  return(
+    <div id='container'>
+      <HomePageBtn />
     </div>
   )}
