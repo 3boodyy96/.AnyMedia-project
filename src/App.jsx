7189f3bb686ffeb1025fda57e2fb82d9751602fd
3boodyy96 <3boodyy96@protonmail.com>
2026-05-13 15:01:22 +0300

    final touches

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,6 @@ import HomePageBtn from "./components/HomePageBtn.jsx";
 export default function App() {
   return(
     <div id='container'>
-      <HomePageBtn />
+      <HomePageBtn scale="scale-40" />
     </div>
   )}
