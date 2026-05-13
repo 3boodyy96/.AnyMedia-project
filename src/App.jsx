c266e0aee842b977b366b43552482a636c2c0270
3boodyy96 <3boodyy96@protonmail.com>
2026-05-13 15:31:42 +0300

    final final touches 🥀✌

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,6 @@ import HomePageBtn from "./components/HomePageBtn.jsx";
 export default function App() {
   return(
     <div id='container'>
-      <HomePageBtn scale="scale-40" />
+      <HomePageBtn scale="scale-40" url="/" />
     </div>
   )}
--- src/components/HomePageBtn.jsx
+++ src/components/HomePageBtn.jsx
@@ -1,8 +1,10 @@
-function HomePageBtn({scale=scale}) {
+function HomePageBtn({scale=scale,url=url}) {
     return (
         <div id="container">
             <div className="flex flex-row items-center justify-center">
-                <img src="../src/assets/home-page-btn.png" alt="Website Icon" className={`${scale ?? ''}`}/>
+                <a href={`${url}`}>
+                    <img src="../src/assets/home-page-btn.png" alt="Website Icon" className={`${scale ?? ''}`}/>
+                </a>
             </div>
         </div>
     )
