b30606c4c5ee8a69c3899df5645c6dc71a93ce43
3boodyy96 <3boodyy96@protonmail.com>
2026-05-21 15:48:12 +0300

    added some features for the animation

--- src/components/checkbox.jsx
+++ src/components/checkbox.jsx
@@ -1,24 +1,34 @@
 import { useState } from "react";
 
 function Checkbox({ onChange }) {
-    const [checked, setChecked] = useState(false);
-    function handleClick() {
-        setChecked(!checked);
-        if (onChange) onChange(!checked);
-    }
-    return (
-        <div
-            className="relative border-2 border-white w-10 h-10 cursor-pointer overflow-visible"
-            onClick={handleClick}
-        >
-            {checked && (
-                <img
-                    src="../src/assets/checkmark.png"
-                    alt="checkMark"
-                    className="absolute w-16 h-16 -bottom-1 -right-2 scale-200 object-contain"
-                />
-            )}
-        </div>
-    );
+  const [checked, setChecked] = useState(false);
+  function handleClick() {
+    setChecked(!checked);
+    if (onChange) onChange(!checked);
+  }
+  return (
+    <div
+      onClick={handleClick}
+      className="
+            overflow-visible
+            w-10 h-10
+            border-2 border-white
+            cursor-pointer
+            relative
+          "
+    >
+
+        <img
+          src="../src/assets/checkmark.png"
+          alt="checkMark"
+          className={`
+                    object-contain
+                    w-16 h-16
+                    absolute -bottom-1 -right-2 scale-200 transition-opacity duration-100
+                    ${checked ? "opacity-100" : "opacity-0"}
+                  `}
+        />
+    </div>
+  );
 }
 export default Checkbox;
