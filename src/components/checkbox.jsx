cf0db077ac07753cc21dcbdab83bec89c6cec0b1
3boodyy96 <3boodyy96@protonmail.com>
2026-05-21 15:33:16 +0300

    fixed the checkbox component

--- src/components/checkbox.jsx
+++ src/components/checkbox.jsx
@@ -1,15 +1,24 @@
-function Checkbox({ label, name, checked, onChange }) {
+import { useState } from "react";
+
+function Checkbox({ onChange }) {
+    const [checked, setChecked] = useState(false);
+    function handleClick() {
+        setChecked(!checked);
+        if (onChange) onChange(!checked);
+    }
     return (
-        <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
-            <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
-                <label class="btn btn-outline-primary" for="btncheck1">Checkbox 1</label>
-
-                <input type="checkbox" class="btn-check" id="btncheck2" autocomplete="off">
-                    <label class="btn btn-outline-primary" for="btncheck2">Checkbox 2</label>
-
-                    <input type="checkbox" class="btn-check" id="btncheck3" autocomplete="off">
-                        <label class="btn btn-outline-primary" for="btncheck3">Checkbox 3</label>
-                    </div>
-                    )
+        <div
+            className="relative border-2 border-white w-10 h-10 cursor-pointer overflow-visible"
+            onClick={handleClick}
+        >
+            {checked && (
+                <img
+                    src="../src/assets/checkmark.png"
+                    alt="checkMark"
+                    className="absolute w-16 h-16 -bottom-1 -right-2 scale-200 object-contain"
+                />
+            )}
+        </div>
+    );
 }
-                    export default Checkbox;
+export default Checkbox;
