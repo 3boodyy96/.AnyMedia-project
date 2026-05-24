b99380be896fc83bf14586beed5a7b4cc3a6a6bf
3boodyy96 <3boodyy96@protonmail.com>
2026-05-24 05:19:38 +0300

    testing backend 🥀

---
+++ help/backEnd/controllers/example.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv, dotenv_values
+from sqlalchemy import Column, String, Integer, create_engine, Sequence, ForeignKey
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+import sqlalchemy as sa
+
+load_dotenv("C:\\Users\\PC\\Documents\\programming\\.AnyMedia-project\\help\\backEnd\\.env\\main.py")
+engine = create_engine(os.getenv("DB_URL"))
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+
+class Users(Base):
+    __tablename__ = "users"
+    id = Column(Integer, Sequence("user_id_seq"), nullable=False, primary_key=True)
+    name = Column(String(50), nullable=True)
+    UserName = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+
+Base.metadata.create_all(engine)
+
+test = Users(id=1, name="3boodyy96", UserName="3boodyy96", email="test", password="123456")
+
+session.add(test)
+session.commit()
