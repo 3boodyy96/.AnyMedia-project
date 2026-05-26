e283244ae8b33db0a7528a9ad2c0f0b0a587c730
3boodyy96 <3boodyy96@protonmail.com>
2026-05-26 08:32:22 +0300

    game over 💔 ... but never lose hope ... continue?

--- help/backEnd/auth/JWT.py
+++ help/backEnd/auth/JWT.py
@@ -14,3 +14,22 @@ header = {
 
 
 security = HTTPBearer()
+
+# no coding today just pain 🥀
+# staring at the screen and getting dizzy by doing nothing
+# shame
+# thats not me
+# im stronger than this shit called (back end)
+# but it seems like its not "back end" but "black end"
+# idk what to say
+# still determined but need to recharge the battery 🥀😵
+# could i refuse?
+# like yk i put all my hopes and dreams in my recycle bin
+# im not lying btw 🥀
+# https://files.catbox.moe/ownzpt.png
+# ...
+# if you read this
+# then you are jobless
+# touch a grass plz and leave me suffering alone
+# ...
+# maybe some "yume nikki" would fix me and make me feel better
