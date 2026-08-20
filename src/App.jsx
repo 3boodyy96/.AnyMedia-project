234307d0a84d4b7bf3b81f7f806b5677d08e2e74
AbdullahBuhlaq <3bdullah.bu7la8@gmail.com>
2026-08-20 10:47:31 +0300

    init the homepages files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import VerficationCodePage from "./pages/verfication-code-page.jsx"
-import DropDownMenu from "./components/DropDownMenu.jsx"
 export default function App() {
   return (
     <div>
       <VerficationCodePage />
-      <DropDownMenu />
 
     </div>
   )
